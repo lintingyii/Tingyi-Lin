9fc70e910411e13c24ddf166a26b697131631006
林庭奕 <lintingyi@lintingyideMacBook-Pro.local>
2023-05-11 23:01:07 +0800

    Fix the checkDevice mobile problem

--- try.js
+++ try.js
@@ -1,18 +1,14 @@
-// function checkDevice() {
-//   var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-//   if (isMobile) {
-//     setInterval(function() {
-//         alert("請使用電腦瀏覽");
-//     }, 1000); // 每隔1秒弹出一次提示框
-//   }
-// }
 function checkDevice() {
   var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   if (isMobile) {
-      alert("請使用電腦進行體驗");
+    setInterval(function() {
+        alert("請使用電腦瀏覽");
+    }, 1000); // 每隔1秒弹出一次提示框
   }
 }
-
+document.addEventListener("DOMContentLoaded", function() {
+  checkDevice();
+});
 window.addEventListener('scroll', function() {
     let img = document.querySelector('.container img');
     let scrollPosition = window.scrollY;
